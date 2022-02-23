47976925982e7e4969b9ca63b6076fb8fd5c20ee
SYdney-Go <ksy960809@gmail.com>
2022-02-23 09:23:22 +0900

    possible?

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,175 @@
+var store = [{
+        "title": "Chapter 1 | 프로그램과 C언어",
+        "excerpt":"1. 프로그래밍 기초 프로그램이란? 컴퓨터에게 작업할 일을 순서대로 알려주기 위해 만든 파일 ex) 실행 파일 : 컴퓨터가 작업할 내용을 컴퓨터가 이해할 수 있는 언어로 작성됨 프로그래밍 언어란? 사람들에게 친숙한 표현법으로 프로그래밍 할 수 있게 만들어진 언어 이 언어로 작업한 내용을 컴파일러를 이용하여 기계어로 번역 2. C 언어 소개 C언어의 탄생...","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_1/",
+        "teaser": null
+      },{
+        "title": "Chapter 10 | 시프트 연산자와 비트 연산자",
+        "excerpt":"      1. C 언어와 함수         2. 함수 정의하고 호출하기         3. main 함수 정리하기         4. 함수 원형 선언하기  ","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_10/",
+        "teaser": null
+      },{
+        "title": "Chapter 11 | 지역 변수와 전역함수",
+        "excerpt":"      1. C 언어와 함수         2. 함수 정의하고 호출하기         3. main 함수 정리하기         4. 함수 원형 선언하기  ","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_11/",
+        "teaser": null
+      },{
+        "title": "Chapter 12 | 배열과 문자열",
+        "excerpt":"      1. C 언어와 함수         2. 함수 정의하고 호출하기         3. main 함수 정리하기         4. 함수 원형 선언하기  ","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_12/",
+        "teaser": null
+      },{
+        "title": "Chapter 13.1 | 운영체제의 메모리 관리 방식",
+        "excerpt":"1. 운영체제와 프로그래밍 C언어 소스코드의 모든 변수들은 컴파일하면 모두 메모리 주소로 바뀐다! 즉, 기계어에서는 변수를 사용하지 않아도 변수의 주소만 알아도 변수의 값을 읽거나 바꿀수 있다! 이런 컴퓨터 시스템의 메모리를 관리하는건 운영체제이다! 2. 메모리 주소 지정 방식 운영체제들은 메모리 주소를 1바이트 단위로 관리한다. 그렇기 때문에 메모리를 사용하려면 사용할 메모리의 주소와 사용할...","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_13.1/",
+        "teaser": null
+      },{
+        "title": "Chapter 13.2 | 포인터",
+        "excerpt":"      1. 운영체제의 메모리 관리 방식         2. 포인터         3. 포인터와 const 키워드         4. 포인터 변수의 주소 연산         5. 포인터와 대상의 크기         6. void* 형 포인터   ","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_13.2/",
+        "teaser": null
+      },{
+        "title": "Chapter 13.3 | 포인터와 const",
+        "excerpt":"      1. 운영체제의 메모리 관리 방식         2. 포인터         3. 포인터와 const 키워드         4. 포인터 변수의 주소 연산         5. 포인터와 대상의 크기         6. void* 형 포인터   ","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_13.3/",
+        "teaser": null
+      },{
+        "title": "Chapter 13.4 | 포인터 변수의 주소 연산",
+        "excerpt":"      1. 운영체제의 메모리 관리 방식         2. 포인터         3. 포인터와 const 키워드         4. 포인터 변수의 주소 연산         5. 포인터와 대상의 크기         6. void* 형 포인터   ","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_13.4/",
+        "teaser": null
+      },{
+        "title": "Chapter 13.5 | 포인터와 대상의 크기",
+        "excerpt":"      1. 운영체제의 메모리 관리 방식         2. 포인터         3. 포인터와 const 키워드         4. 포인터 변수의 주소 연산         5. 포인터와 대상의 크기         6. void* 형 포인터   ","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_13.5/",
+        "teaser": null
+      },{
+        "title": "Chapter 13.6 | void* 형 포인터",
+        "excerpt":"      1. 운영체제의 메모리 관리 방식         2. 포인터         3. 포인터와 const 키워드         4. 포인터 변수의 주소 연산         5. 포인터와 대상의 크기         6. void* 형 포인터   ","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_13.6/",
+        "teaser": null
+      },{
+        "title": "Chapter 14 | 표준 입력 함수",
+        "excerpt":"      1. C 언어와 함수         2. 함수 정의하고 호출하기         3. main 함수 정리하기         4. 함수 원형 선언하기  ","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_14/",
+        "teaser": null
+      },{
+        "title": "Chapter 15 | 배열과 포인터",
+        "excerpt":"      1. C 언어와 함수         2. 함수 정의하고 호출하기         3. main 함수 정리하기         4. 함수 원형 선언하기  ","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_15/",
+        "teaser": null
+      },{
+        "title": "Chapter 16 | 메모리 할당",
+        "excerpt":"      1. C 언어와 함수         2. 함수 정의하고 호출하기         3. main 함수 정리하기         4. 함수 원형 선언하기  ","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_16/",
+        "teaser": null
+      },{
+        "title": "Chapter 17 | 다차원 포인터",
+        "excerpt":"      1. C 언어와 함수         2. 함수 정의하고 호출하기         3. main 함수 정리하기         4. 함수 원형 선언하기  ","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_17/",
+        "teaser": null
+      },{
+        "title": "Chapter 18 | 구조체와 연결리스트",
+        "excerpt":"      1. C 언어와 함수         2. 함수 정의하고 호출하기         3. main 함수 정리하기         4. 함수 원형 선언하기  ","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_18/",
+        "teaser": null
+      },{
+        "title": "Chapter 19 | 파일 입출력",
+        "excerpt":"      1. C 언어와 함수         2. 함수 정의하고 호출하기         3. main 함수 정리하기         4. 함수 원형 선언하기  ","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_19/",
+        "teaser": null
+      },{
+        "title": "Chapter 2 | C언어로 만드는 첫 번째 프로그램",
+        "excerpt":"1. 내가 만든 첫 번째 프로그램 #include &lt;stdion.h&gt; void main(void) { printf(\"Hello World!\"); } #include &#60;stdio.h&#62; 전처리기 : #으로 시작하는 문장 이 예제 소스 코드 번역 전에 stdio.h 파일을 먼저 참조해줘! main 프로그램의 시작을 알려주는 함수 void 함수 앞 void : main 함수의 수행 결과 값이 정해지지 않았다. 함수 뒤...","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_2/",
+        "teaser": null
+      },{
+        "title": "Chapter 20 | 함수 포인터",
+        "excerpt":"      1. C 언어와 함수         2. 함수 정의하고 호출하기         3. main 함수 정리하기         4. 함수 원형 선언하기  ","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_20/",
+        "teaser": null
+      },{
+        "title": "Chapter 3 | 자료형",
+        "excerpt":"1. 컴퓨터의 자료 기억 방식 메모리 컴퓨터의 주기억 장치 1바이트 단위로 주소가 매겨져 있고, 운영체제가 이들 주소를 1바이트 단위로 관리한다. 1비트 0 또는 1을 저장할 수 있는 공간 저장단위가 1비트 증가할때, 저장단위는 2배로 늘어난다. 부호비트 비트 1개에 음수인지 양수인지의 상태를 저장 메모리 절약 데이터의 유효범위를 예상하여 적합한 크기를 결정한 뒤,...","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_3/",
+        "teaser": null
+      },{
+        "title": "Chapter 4 | 상수와 변수",
+        "excerpt":"1. 상수 프로그램 실행에서부터 종료까지 변하지 않는 값 숫자형 상수 정수형 상수 실수형 상수 정수형 상수의 진법 표현 8진수 : 앞에 숫자 0을 붙여서 표현 16진수 : 앞에 0x를 붙여서 표현 문자형 상수 작은 따옴표를 사용하여 표현 영문자 숫자 특수 문자 문자열형 상수 큰따옴표를 사용하여 표현 끝을 표현하기 위해 마지막...","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_4/",
+        "teaser": null
+      },{
+        "title": "Chapter 5 | 함수",
+        "excerpt":"1. C 언어와 함수 함수란? 작업 단위로 명령들을 묶어서 하나의 그룹으로 만든 것 main 함수 컴파일러가 프로그램의 시작 함수로 인식하는 특별한 함수 2. 함수 정의하고 호출하기 함수 정의하기 함수 이름 매개 변수 작업 내용 반환값 : C는 한번에 하나의 값만 돌려줄 수 있다. 함수 호출하기 함수명을 사용하여 호출한다. 함수 매개변수...","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_5/",
+        "teaser": null
+      },{
+        "title": "Chapter 6.1 | 라이브러리와 헤더파일",
+        "excerpt":"1. 라이브러리 (1) 라이브러리란? 만들어진 함수들 중 내용이 거의 바뀌지 않는 것들을 따로 관리하기 위해 분리한 소스 코드. ① 등장 이유 소스 파일 중 변경 가능성이 없는 파일을 따로 보관, 목적 파일을 재사용 소스 파일 전체를 컴파일 하는 것보다 효율적이다. 이때, 목적 파일에서 실제 사용되는 함수의 기계어만 분리하여 만든 것이...","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_6.1/",
+        "teaser": null
+      },{
+        "title": "Chapter 6.2 | 표준 출력 함수 1",
+        "excerpt":"      1. 표준 출력 함수  해당 시스템이 가장 기본으로 사용하는 출력 방식 → 모니터   (1) putchar : 단일 문자 출력 함수 (줄바꿈 X)   #include &lt;stdio.h&gt;  void main() {     putchar('H');     putchar('i');     putchar('~'); }   (2) puts : 문자열 출력 함수 (줄바꿈 O)   #include &lt;stdio.h&gt;  void main() {     puts(\"Hi~\"); }        ","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_6.2/",
+        "teaser": null
+      },{
+        "title": "Chapter 6.3 | 표준 출력 함수 2",
+        "excerpt":"1. 문자열 출력 함수 printf (1) 문자열 서식 %d : 10진수 정수 %f : 실수 %c : 문자 # include &lt;stdio.h&gt; void main() { printf(\"65의 ASCII 값은 %c입니다.\", 65); /*결과 : 65의 ASCII 값은 A입니다.*/ } %s : 문자열 %u : 부호를 고려하지 않는 10진수 정수 %o : 8진수 형태로...","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_6.3/",
+        "teaser": null
+      },{
+        "title": "Chapter 7.1 | 연산자",
+        "excerpt":"1. 대입 연산자 대입 연산자를 이용하여 변수에 값을 저장할 수 있다. #include &lt;stdio.h&gt; void main() { int data1, data2; data1 = 5; data2 = data1; printf(\"data1 = %d, data2 = %d\\n\", data1, data2); /*결과 : data1 = 5, data2 = 5*/ } 2. 산술 연산자 #include &lt;stdio.h&gt; void main() {...","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_7.1/",
+        "teaser": null
+      },{
+        "title": "Chapter 7.2 | 연산자 우선 순위",
+        "excerpt":"1. 연산 우선순위 괄호, 배열, 구조체 : (), [] 단항 연산자 : *, &amp;, ++, –, +, -, sizeof 승제 연산자 : *, /, % 가감 연산자 : +, - 시프트 연산자 : «, » 비교 연산자 : &lt;, &gt;, &lt;=, &gt;= 등가 연산자 : ==, != 비트 연산자 AND :...","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_7.2/",
+        "teaser": null
+      },{
+        "title": "Chapter 8 | 조건문",
+        "excerpt":"      1. C 언어와 함수         2. 함수 정의하고 호출하기         3. main 함수 정리하기         4. 함수 원형 선언하기  ","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_8/",
+        "teaser": null
+      },{
+        "title": "Chapter 9 | 반복문",
+        "excerpt":"      1. C 언어와 함수         2. 함수 정의하고 호출하기         3. main 함수 정리하기         4. 함수 원형 선언하기  ","categories": ["Do it! C언어 입문"],
+        "tags": [],
+        "url": "/do%20it!%20c%EC%96%B8%EC%96%B4%20%EC%9E%85%EB%AC%B8/Doit_C_9/",
+        "teaser": null
+      },{
+        "title": "왜 포인터를 쓰는가?",
+        "excerpt":"      왜 포인터를 쓰지? 그냥 값을 가져다가 쓸 수도 있잖아!  c의 구조체 안에 함수를 넣어서 쓸때, 함수 포인터로만 할 수 있다   그리고 Class를 왜 쓰지? 그냥 함수 가져다가 쓸 수도 있잖아!  class에서 상속할때 형변환과 관련이 있다  class의 self들이 pointer와 비슷하다…  ","categories": ["숙제"],
+        "tags": [],
+        "url": "/%EC%88%99%EC%A0%9C/%ED%8F%AC%EC%9D%B8%ED%84%B0/",
+        "teaser": null
+      }]
